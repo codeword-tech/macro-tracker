db48a531ff52a2d66224ad0d32539591fa7ac461
Saurabh <saurabh@codeword.tech>
2023-09-19 16:00:47 +0530

    fix: recursion

--- src/sessionId.ts
+++ src/sessionId.ts
@@ -2,6 +2,12 @@ import { SESSION_COOKIE_KEY } from './constants'
 import { getCookie, setCookie } from './cookie'
 import { uuidv4 } from './uuidv4'
 
+/**
+ * Try to keep same session id if session cookie exists, generate a new one otherwise.
+ *   - First request in a session will generate a new session id
+ *   - The next request will keep the same session id and extend the TTL for 30 more minutes
+ */
+
 export function getSessionIdOrCreate() {
   const sessionId = getCookie(SESSION_COOKIE_KEY)
 
@@ -13,17 +19,6 @@ export function getSessionIdOrCreate() {
 }
 
 export function createSessionId() {
-  /**
-   * Try to keep same session id if session cookie exists, generate a new one otherwise.
-   *   - First request in a session will generate a new session id
-   *   - The next request will keep the same session id and extend the TTL for 30 more minutes
-   */
-  const lastSessionId = getSessionIdOrCreate()
-
-  if (lastSessionId) {
-    return lastSessionId
-  }
-
   const sessionId = uuidv4()
   setCookie(SESSION_COOKIE_KEY, sessionId)
 
